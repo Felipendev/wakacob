bb03d5bf531d9af99c9d97946ee2d297a6e5ae27
til_fpedroso <til_fpedroso@uolinc.com>
2024-05-14 12:27:53 -0300

    Implements flow repository pauta

--- src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -1,17 +1,23 @@
 package academy.wakanda.wakacop.pauta.domain;
 
+import academy.wakanda.wakacop.pauta.application.api.request.NovaPautaRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Pauta {
 
     @Id
@@ -22,4 +28,11 @@ public class Pauta {
     private String descricao;
     private UUID idAssociadoAutor;
     private LocalDateTime dataCriacao;
+
+    public Pauta(NovaPautaRequest novaPauta) {
+        this.titulo = novaPauta.getTitulo();
+        this.descricao = novaPauta.getDescricao();
+        this.idAssociadoAutor = novaPauta.getIdAssociadoAutor();
+        this.dataCriacao = LocalDateTime.now();
+    }
 }
