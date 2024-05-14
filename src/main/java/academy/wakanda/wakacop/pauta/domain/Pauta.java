5f249967766f14224421d7d65bfdca58e1c34bad
til_fpedroso <til_fpedroso@uolinc.com>
2024-05-14 06:51:46 -0300

    Implement Pauta domain

--- src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -1,4 +1,25 @@
 package academy.wakanda.wakacop.pauta.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
 public class Pauta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    private String titulo;
+    private String descricao;
+    private UUID idAssociadoAutor;
+    private LocalDateTime dataCriacao;
 }
