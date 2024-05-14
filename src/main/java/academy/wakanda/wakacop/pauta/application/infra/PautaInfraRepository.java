2a473cdc17ce9cf452346c817aba527fc89f1d23
til_fpedroso <til_fpedroso@uolinc.com>
2024-05-14 12:28:01 -0300

    Implements flow repository pauta

---
+++ src/main/java/academy/wakanda/wakacop/pauta/application/infra/PautaInfraRepository.java
@@ -0,0 +1,25 @@
+package academy.wakanda.wakacop.pauta.application.infra;
+
+import academy.wakanda.wakacop.pauta.application.repository.PautaRepository;
+import academy.wakanda.wakacop.pauta.domain.Pauta;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.antlr.v4.runtime.misc.LogManager;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class PautaInfraRepository implements PautaRepository {
+    private final PautaSpringDataJPARepository pautaSpringDataJPARepository;
+
+    @Override
+    public Pauta salva(Pauta pauta) {
+        log.info("[start] - PautaInfraRepository - salva");
+        log.info("[Pauta] {}", pauta);
+        pautaSpringDataJPARepository.save(pauta);
+        log.info("[finish] - PautaInfraRepository - salva");
+        return pauta;
+    }
+}
